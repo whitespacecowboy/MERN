b50f4c486b1424c09d063f083f4766c001f9c7d7
jagbir8790-arch <jagbir8790@gmail.com>
2026-07-10 13:05:25 +0530

    Add table

--- 8-July/Task/route/connection.js
+++ 8-July/Task/route/connection.js
@@ -17,39 +17,22 @@ app.get("/", (req, res) => {
 
 app.post("/add", async (req, res) => {
 	await addBooking(req.body)
-	console.log(req.body)
 	res.redirect("/success")
-    res.status(201)
-	res.end()
 });
 
 app.post("/search", async (req, res) => {
-	console.log(req.body)
 	const result = await searchBooking({ bookingId: Number(req.body.searchId) })
 	res.send(result)
-    res.status(200)
-	res.end()
 });
 
 app.get("/success", (req, res) => {
 	res.send("Added successfully")
-	res.end()
 })
 
 app.get("/viewall", async (req, res) => {
 	const output = await searchBooking()
-	// for (v of output) {
-	// 	res.send(v)	
-	// }
 	res.send(output)
-    res.status(200)
-	res.end()
 });
-
-// app.get("/*", async (req, res) => {
-//     res.send("404")
-//     res.status(404)
-// });
 
 app.listen(PORT, () => {
 	console.info(`Server URL: http://localhost:${PORT}`);
